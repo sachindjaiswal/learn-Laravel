ca3455c89c09151fccb954a35bbd86478278d4a2
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-07-22 19:00:33 +0530

    Started with model query builder

--- first-project/app/Http/Controllers/Student.php
+++ first-project/app/Http/Controllers/Student.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\student as ModelsStudent;
 use Illuminate\Http\Request;
 
 class Student extends Controller
 {
     //
-    function getStudentDetails(){
+//     function getStudentDetails(){
 
-        $students = \App\Models\student::all();
-        return view('student' , ['Students'=>$students]);
+//         $students = \App\Models\student::all();
+//         return view('student' , ['Students'=>$students]);
+//     }
+
+    function getStudentDetails(){
+        //  $response = ModelsStudent::get();
+        $response = ModelsStudent::where('first_name','John')->update(['last_name'=>"jaiswal"]);
+        $response = [$response];
+        return $response ;
     }
 }
